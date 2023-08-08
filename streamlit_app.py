eb9584370526fa0359b3d1d492d02d512b9a97c8
srikantha534 <141729774+srikantha534@users.noreply.github.com>
2023-08-08 16:20:24 +0530

    Update streamlit_app.py
    additional text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("Hello world");
+streamlit.header("2-Hello world");
+streamlit.text("3-Hello world");
+streamlit.text("4-Hello world");
+streamlit.text("5-Hello world");
